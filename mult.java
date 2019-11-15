036ef0031a776e28ab71298b999ae98504f60f2c
PushpenderRawat12345 <pushpender.rawat@st.niituniversity.in>
2019-11-15 17:17:25 +0530

    multiplication

---
+++ mult.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class mult
+{
+	Scanner sc = new Scanner(System.in);
+	System.out.println("enter 1st number");
+	int a = sc.nextInt();
+	System.out.println("enter 2nd number");
+	int b = sc.nextInt();
+	System.out.println("multiplication of two numbers is :", a*b);
+}
+
+
